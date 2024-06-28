3b246ceebf5968681eb72febaf4ba972501c9955
EduardoSSiqueira <eduardosouto2014@gmail.com>
2024-06-27 23:10:23 -0300

    Atualização

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
-from fastapi import FastAPI
+from re import template
+from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 import uvicorn
-
+from routes.RootRouter import router as rootRouter
 app = FastAPI()
 
 app.mount(path="/static", app=StaticFiles(directory="static"),name="static")
 
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+app.include_router(rootRouter)
+
+
 if __name__=="__main__":
     uvicorn.run(app="main:app", reload=True)
+
+
     
---
+++ routes/RootRouter.py
@@ -0,0 +1,11 @@
+from fastapi import APIRouter, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
+router = APIRouter()
+templates= Jinja2Templates(directory="templates")
+
+@router.get("/", response_class=HTMLResponse)
+
+async def get_root( request:Request,):
+    return templates.TemplateResponse("root/Cadastro_user.html",{"request": request},)
